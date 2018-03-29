0fbe80fd38c7cb736c33107070479a11732d1d3a
Samuel Sapp <sappsoftware.samuel@gmail.com>
2018-03-28 23:14:53 -0400

    ok but really

--- conf.lua
+++ conf.lua
@@ -1,12 +1,9 @@
 function love.conf(t)
   t.title = "Math Cup"
   t.version = "0.10.2"
-  t.window.width = 1920
-  t.window.height = 1080
   t.window.resizable = false
   t.window.minwidth = 1024
   t.window.minheight = 768
   t.console = false
-  t.window.fullscreen = false
-  t.window.borderless = true
+  t.window.fullscreen = true
 end
